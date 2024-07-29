282be30a5c8a54f2984a23ab1ff71c1e3f88c91f
Alejo Perez <alejo.perez2905@gmail.com>
2024-07-29 10:22:37 -0500

    The product entity is created

---
+++ src/main/java/com/riwi/springboot_filter_teams/domain/entities/Product.java
@@ -0,0 +1,21 @@
+package com.riwi.springboot_filter_teams.domain.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "product")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private double price;
+}
