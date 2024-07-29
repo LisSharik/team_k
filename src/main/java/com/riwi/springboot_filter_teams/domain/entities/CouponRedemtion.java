13d51d74131195f4d14a045c25f566d8faadccb3
XtianCarde <117883355+XtianCarde@users.noreply.github.com>
2024-07-29 10:39:07 -0500

    Update CouponRedemtion.java

--- src/main/java/com/riwi/springboot_filter_teams/domain/entities/CouponRedemtion.java
+++ src/main/java/com/riwi/springboot_filter_teams/domain/entities/CouponRedemtion.java
@@ -7,14 +7,18 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
-@Entity(name = "cupon_redemtions")
+@Entity(name = "cupon_redemptions")
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 public class CouponRedemtion {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private LocalDateTime dateRedemtion;
 }
